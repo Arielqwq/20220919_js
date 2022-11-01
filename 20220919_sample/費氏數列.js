7967395eaeb5f40ccc5a37e66f2ded3cb3d1c4f3
Ariel <dawei4102@gmail.com>
2022-11-01 21:55:34 +0800

    20220919_js
    20220919_js

---
+++ 20220919_sample/費氏數列.js
@@ -0,0 +1,39 @@
+max = 22
+first = 0
+second = 1
+
+while(0 <= 22)
+document.write(first) = 0
+let temp = first = 0
+first = second = 1
+second = first + temp = 1 + 0 = 1
+
+first = 1
+second = 1
+
+while(1 <= 22)
+document.write(first) = 1
+let temp = first = 1
+first = second = 1
+second = first + temp = 1 + 1 = 2
+
+first = 1
+second = 2
+
+while(2 <= 22)
+document.write(first) = 1
+let temp = first = 1
+first = second = 2
+second = first + temp = 2 + 1 = 3
+
+first = 2
+second = 3
+
+while(3 <= 22)
+document.write(first) = 2
+let temp = first = 2
+first = second = 3
+second = first + temp = 3 + 2 = 5
+
+first = 3
+second = 5
